927ac33d5952d3f50cd4c1965dde31daf58cf8c4
Harsh Thakur <harshprojects2002@outlook.com>
2026-07-19 14:27:36 +0530

    chore: make eslint ignore shadcn components files generated by cli

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -36,6 +36,8 @@ export default defineConfig(
 	{
 		// Override or add rule settings here, such as:
 		// 'svelte/button-has-type': 'error'
-		rules: {}
+	},
+	{
+		ignores: ['src/lib/components/ui/**']
 	}
 );
